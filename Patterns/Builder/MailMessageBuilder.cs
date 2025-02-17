55644d2c0e44150fa9faf51aacae02631734a630
teafr <tookyoumy7@gmail.com>
2025-02-17 10:13:04 +0200

    Made MailMessage Builder and built it in class Program

---
+++ Patterns/Builder/MailMessageBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class MailMessageBuilder
+    {
+        private readonly MailMessage _mailMessage = new MailMessage();
+
+        public MailMessageBuilder From(string mailAdress) 
+        { 
+            _mailMessage.From = new MailAddress(mailAdress); 
+            return this;
+        }
+
+        public MailMessageBuilder To(string mailAdress)
+        {
+            _mailMessage.To.Add(new MailAddress(mailAdress));
+            return this;
+        }
+
+        public MailMessageBuilder Cc(string cc)
+        {
+            _mailMessage.CC.Add(cc);
+            return this;
+        }
+
+        public MailMessageBuilder Subject(string subject, Encoding encoding)
+        {
+            _mailMessage.Subject = subject;
+            _mailMessage.SubjectEncoding = encoding;
+            return this;
+        }
+
+        public MailMessageBuilder Body(string body, Encoding encoding)
+        {
+            _mailMessage.Body = body;
+            _mailMessage.BodyEncoding = encoding;
+            return this;
+        }
+
+        public MailMessage Build()
+        {
+            return _mailMessage;
+        }
+    }
+}
---
+++ Patterns/Builder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+using System.Text;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mailMessage = new MailMessageBuilder().From("from@dot.net")
+                                                              .To("to@dot.net")
+                                                              .Cc("cc@dot.net")
+                                                              .Subject("Information", Encoding.UTF8)
+                                                              .Body("Hello world!", Encoding.UTF8)
+                                                              .Build();
+        }
+    }
+}
